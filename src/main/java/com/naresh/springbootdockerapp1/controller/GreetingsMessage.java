34c87a8d121099b98a5fab7c67a95cafe3dd805d
nareshavula2424 <nareshavula2424@gmail.com>
2024-02-20 17:08:44 +0530

    change method type

--- src/main/java/com/naresh/springbootdockerapp1/controller/GreetingsMessage.java
+++ src/main/java/com/naresh/springbootdockerapp1/controller/GreetingsMessage.java
@@ -4,17 +4,14 @@ import com.naresh.springbootdockerapp1.service.MessageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
 public class GreetingsMessage {
     @Autowired
     public MessageService messageService;
-    @PostMapping("/{name}")
+    @GetMapping("/{name}")
     public ResponseEntity<String> getMessage(@PathVariable String name){
         return new ResponseEntity<>(messageService.getMessage(name), HttpStatus.OK);
 
